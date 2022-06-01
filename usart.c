8b441fb91745c7fb4ea0ee5d6a8b94044efb6a8b
justmalysa <justynamalysa98@gmail.com>
2022-06-01 21:45:36 +0200

    Fix usart library

--- usart.c
+++ usart.c
@@ -13,7 +13,7 @@
 #define USARTx                           USART1
 #define USARTx_CLK_ENABLE()              __USART1_CLK_ENABLE();
 #define USARTx_RX_GPIO_CLK_ENABLE()      __GPIOA_CLK_ENABLE()
-#define USARTx_TX_GPIO_CLK_ENABLE()      __GPIOA_CLK_ENABLE() 
+#define USARTx_TX_GPIO_CLK_ENABLE()      __GPIOB_CLK_ENABLE()
 
 #define USARTx_FORCE_RESET()             __USART1_FORCE_RESET()
 #define USARTx_RELEASE_RESET()           __USART1_RELEASE_RESET()
